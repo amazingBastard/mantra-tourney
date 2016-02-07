b59c3ab9a627f768347ebc36b2f96ae359c533c5
Damir <damir@withpulp.com>
2016-02-07 06:30:01 -0500

    updates to styles

--- client/modules/core/components/toolbar.jsx
+++ client/modules/core/components/toolbar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Toolbar = () => (
   <aspect className="toolbar aspect">
-    <a className="circular primary icon button" href="/new-post">
-      <i class="fa fa-plus"></i>
+    <a className="icon button" href="/new-post">
+      <i className="fa fa-plus"></i>
     </a>
   </aspect>
 );
