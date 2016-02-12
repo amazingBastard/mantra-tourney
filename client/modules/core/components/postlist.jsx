bbf2c400ba3e9e597a7e9a839d34693175e26e91
Damir <damir@withpulp.com>
2016-02-12 02:08:26 -0500

    post cards markup

--- client/modules/core/components/postlist.jsx
+++ client/modules/core/components/postlist.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
 const PostList = ({posts}) => (
-  <div className='postlist'>
-    <ul>
-      {posts.map(post => (
-        <li key={post._id}>
+  <aspect className='postlist aspect'>
+    {posts.map(post => (
+      <figure className="post card" key={post._id}>
+        <a href={`/post/${post._id}`}>
           <h4 className="title">{post.title}</h4>
+
           <h2 className="date">
             02/06/2016 - 02/07/2016
           </h2>
-          <p className="prize">$2,000</p>
+
+          <p className="prize">$2,000 prize</p>
+
+          <p className="players">16 players</p>
+
           <p className="location">Online</p>
-          <a href={`/post/${post._id}`}>more details</a>
-        </li>
-      ))}
-    </ul>
-  </div>
+        </a>
+      </figure>
+    ))}
+  </aspect>
 );
 
 export default PostList;
