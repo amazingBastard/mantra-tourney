20664a986b5f2cdd3f48bd78dd9f26224a7d2916
Damir <damir@withpulp.com>
2016-02-15 15:21:47 -0500

    post component wip

--- client/modules/core/components/post.jsx
+++ client/modules/core/components/post.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import CommentList from '../../comments/containers/comment_list.js';
 
 const Post = ({post}) => (
-  <div>
+  <aspect className="post aspect">
     {post.saving ? <p>Saving...</p> : null}
-    <h2>{post.title}</h2>
-    <p>
+    <h2 className="title">{post.title}</h2>
+    <p className="content">
       {post.content}
     </p>
-    <div>
+    <div className="comments">
       <h4>Comments</h4>
       <CommentList postId={post._id}/>
     </div>
-  </div>
+  </aspect>
 );
 
 export default Post;
