ef0cc04a34c9d4e5fdbc5a20debcd14c22857027
Damir <damir@withpulp.com>
2016-02-17 20:56:56 -0500

    added dochead file

---
+++ client/modules/core/dochead.js
@@ -0,0 +1,20 @@
+let title = 'Tourney.News | News for upcoming Starcraft II tournaments',
+    description = {
+      name: 'description',
+      content: 'A basic web app that scrapes data on upcoming Starcraft II tournaments and lists them for the user in a mobile friendly user interface.'
+    },
+    viewport = {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1'
+    },
+    favicon = {
+      rel: 'icon',
+      sizes: '16x16 32x32',
+      type: 'image/png',
+      href: '/icon.png'
+    };
+
+DocHead.setTitle(title);
+DocHead.addMeta(description);
+DocHead.addMeta(viewport);
+DocHead.addLink(favicon);
